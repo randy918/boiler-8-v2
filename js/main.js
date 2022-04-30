df48b5f7d9840374233e65cfd1ef20eaff8cbfa1
Randy Miller <randy918@gmail.com>
2022-04-30 14:41:18 -0500

    catchup save

--- js/main.js
+++ js/main.js
@@ -1,16 +1,3 @@
-
-$(function() {
-    $(".red-box").hide(2000);
-    
-    $(".green-box").slideUp(2000);
-    $(".green-box").slideDown(2000);
-
-    $(".blue-box").fadeTo(6000, 0.25);
-    $(".red-box").show(2000);
-
-
-
-    
-
-
+$(function () {
+    $(".blue-box").hide(), 
 });
