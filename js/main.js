d2c0d166b9568b6102ff7f7041d6249b97239248
Randy Miller <randy918@gmail.com>
2022-04-29 22:25:25 -0500

    lesson 12 jquery udemy update

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
 
+$(function() {
+    $(".red-box").hide(2000);
+    
+    $(".green-box").slideUp(2000);
+    $(".green-box").slideDown(2000);
+
+    $(".blue-box").fadeTo(6000, 0.25);
+    $(".red-box").show(2000);
+
+
+
+    
+
+
+});
